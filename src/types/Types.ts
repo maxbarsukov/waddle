08cb7891f9ed3af8d82283c6cea409bab72c7610
maxbarsukov <maximbarsukov@bk.ru>
2022-01-07 20:12:43 +0300

    feat: :sparkles: add lang types

---
+++ src/types/Types.ts
@@ -0,0 +1,16 @@
+export const Types = {
+  Object: 'Object',
+  Int: 'Int',
+  Double: 'Double',
+  String: 'String',
+  Bool: 'Bool',
+  Unit: 'Unit',
+  Predef: 'Predef',
+  Null: 'Null',
+  Math: 'Math$',
+  Console: 'Console$',
+};
+
+type Type = string;
+
+export default Type;
