47c727328049ad0d34793343dc4f7df24443bb1c
maxbarsukov <maximbarsukov@bk.ru>
2022-01-08 21:33:36 +0300

    feat: :sparkles: update Initializing node

--- src/ast/expressions/Initialization.ts
+++ src/ast/expressions/Initialization.ts
@@ -3,10 +3,10 @@ import Type from '../../types/Types';
 
 export default class Initialization extends Expression {
   identifier: string;
-  type: Type;
-  value: string;
+  type: Type | undefined;
+  value: Expression | undefined;
 
-  constructor(identifier: string, type: Type, value: string) {
+  constructor(identifier: string, type: Type | undefined, value: Expression | undefined) {
     super();
     this.identifier = identifier;
     this.type = type;
