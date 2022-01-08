6547c133c3f3317a6288cdfc1f6b3efdc3649f1b
maxbarsukov <maximbarsukov@bk.ru>
2022-01-08 21:32:40 +0300

    feat: :sparkles: update IfElse node

--- src/ast/expressions/IfElse.ts
+++ src/ast/expressions/IfElse.ts
@@ -3,9 +3,9 @@ import Expression from '../Expression';
 export default class IfElse extends Expression {
   condition: Expression;
   thenBranch: Expression;
-  elseBranch: Expression;
+  elseBranch: Expression | undefined;
 
-  constructor(condition: Expression, thenBranch: Expression, elseBranch: Expression) {
+  constructor(condition: Expression, thenBranch: Expression, elseBranch: Expression | undefined) {
     super();
     this.condition = condition;
     this.thenBranch = thenBranch;
