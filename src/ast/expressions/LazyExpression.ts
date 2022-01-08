4cef3cf4cc85e6cab41c8cbe4ce6b25f2e0ac9ea
maxbarsukov <maximbarsukov@bk.ru>
2022-01-08 21:33:57 +0300

    feat: :sparkles: create LazyExpression node

---
+++ src/ast/expressions/LazyExpression.ts
@@ -0,0 +1,16 @@
+import Expression from '../Expression';
+
+export default class LazyExpression extends Expression {
+  expression: Expression;
+  context;
+
+  constructor(expression: Expression, context) {
+    super();
+    this.expression = expression;
+    this.context = context;
+  }
+
+  isLazy() {
+    return true;
+  }
+}
