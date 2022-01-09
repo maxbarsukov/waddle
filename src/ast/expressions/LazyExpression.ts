a5b4ff90937f7b01d5632bea4168ab659d1382d7
maxbarsukov <maximbarsukov@bk.ru>
2022-01-09 17:00:38 +0300

    feat: :sparkles: implement Laze Exprs AST node

--- src/ast/expressions/LazyExpression.ts
+++ src/ast/expressions/LazyExpression.ts
@@ -1,13 +1,14 @@
 import Expression from '../Expression';
+import Context from '../../interpreter/Context';
 
 export default class LazyExpression extends Expression {
   expression: Expression;
-  // context;
+  context: Context;
 
-  constructor(expression: Expression) { // , context) {
+  constructor(expression: Expression, context: Context) {
     super();
     this.expression = expression;
-    // this.context = context;
+    this.context = context;
   }
 
   isLazy() {
