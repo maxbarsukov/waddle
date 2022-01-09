c0186187777c24203b5a1a1aa99eb010bf72cfdd
maxbarsukov <maximbarsukov@bk.ru>
2022-01-09 19:30:26 +0300

    feat: :sparkles: update Context types

--- src/interpreter/Context.ts
+++ src/interpreter/Context.ts
@@ -1,18 +1,20 @@
 import Environment from './Environment';
 import Store from './Store';
 import { Class } from '../ast';
+import Obj from './Obj';
+import Env from '../interfaces/Env';
 
-export default class Context {
+export default class Context implements Env {
   classes: Map<string, Class>;
   environment: Environment;
   store: Store;
-  self: Class | undefined;
+  self: Obj | undefined;
 
   constructor(
     classes: Map<string, Class> = new Map<string, Class>(),
     environment = new Environment(),
     store = new Store(),
-    self: Class | undefined = undefined,
+    self: Obj | undefined = undefined,
   ) {
     this.classes = classes;
     this.environment = environment;
