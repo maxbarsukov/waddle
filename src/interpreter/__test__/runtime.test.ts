82431793f3a2458dd8a04076d746fda761dce749
maxbarsukov <maximbarsukov@bk.ru>
2022-01-14 22:39:18 +0300

    test: :rotating_light: update tests for DoubleClass runtime

--- src/interpreter/__test__/runtime.test.ts
+++ src/interpreter/__test__/runtime.test.ts
@@ -56,6 +56,8 @@ describe('Runtime', () => {
       '10 <= 11': true,
       '11 <= 11': true,
       '11 >= 11': true,
+      '11.instanceOf("Int")': true,
+      '11.instanceOf("Double")': false,
     };
 
     Object.entries(testCases).forEach(([source, ans]) => {
@@ -87,6 +89,62 @@ describe('Runtime', () => {
       'true || false': true,
       'false || true': true,
       'false || false': false,
+      'false.instanceOf("Bool")': true,
+      'false.instanceOf("Double")': false,
+    };
+
+    Object.entries(testCases).forEach(([source, ans]) => {
+      it(source, () => {
+        check(source, ans, typeEnv, context);
+      });
+    });
+  });
+
+  describe('DoubleClass', () => {
+    const testCases = {
+      '1.0.toString()': '1',
+      '1.1.toString()': '1.1',
+      '2e2.toString()': '200',
+      '-1.3': -1.3,
+      '-(-1.3)': 1.3,
+      '1.0 == 1.0': true,
+      '1.0 == 1': true,
+      '1.1 == 1.0': false,
+      '1.1 == 1': false,
+      '1.1 == new Object()': false,
+      '1.0 != 1.0': false,
+      '1.0 != 1': false,
+      '1.1 != 1.0': true,
+      '1.1 != 1': true,
+      '1.1 != new Object()': false,
+      '0.1 + 0.2 == 0.3': false,
+      '1.1 + 1.2 == 2.3': true,
+      '1.1 + 2.1 + 3.1 + 4.1 + 5.1': 15.5,
+      '(1.1 + 2.1) * 3.5 + 4.0 + 5.9': 21.1,
+      '1.1 - 10.5': -9.4,
+      '1.1 - 10': -8.9,
+      '10.2 / 2': 5.1,
+      '10.2 / 2.5': 4.08,
+      '10.5 % 3': 1.5,
+      '10.4 * 3': 31.200000000000003,
+      '10.4 * 3.1': 32.24,
+      '3.2.*(2.1)': 6.720000000000001,
+      '0.1 + 0.2': 0.30000000000000004,
+      '2.1e-1': 0.21,
+      '3.1e3': 3100,
+      '3.2E4': 32000,
+      '10.1 > 1': true,
+      '10.1 > 11': false,
+      '10.1 < 1': false,
+      '10.1 < 11.1': true,
+      '10.1 >= 1.1': true,
+      '10.1 >= 11.1': false,
+      '10.1 <= 1.1': false,
+      '10.1 <= 11.1': true,
+      '11.1 <= 11.1': true,
+      '11.1 >= 11.1': true,
+      '1.0.instanceOf("Int")': false,
+      '1.0.instanceOf("Double")': true,
     };
 
     Object.entries(testCases).forEach(([source, ans]) => {
